cca4b9d9ff9860004cf18943e815797114bcf11c
Jesus F. Trujillo <elyeyus@gmail.com>
2013-11-16 20:28:42 -0800

    New yammer login button & oauth server flow

--- yeoman/app/scripts/controllers/login.js
+++ yeoman/app/scripts/controllers/login.js
@@ -1,10 +1,4 @@
 angular.module('foodroulette')
 .controller('LoginCtrl', ['$scope', function ($scope) {
 
-  //Init Yammer login button
-  yam.connect.loginButton('#yammer-login', function (resp) {
-    if (resp.authResponse)
-      document.getElementById('yammer-login').innerHTML = 'Welcome to Yammer!';
-  });
-
 }]);
