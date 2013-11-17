ee1b4f70f7e726f10343ad6931a9959a39d4a2ee
Jesus F. Trujillo <elyeyus@gmail.com>
2013-11-16 16:03:17 -0800

    imin controller and fruser service

---
+++ yeoman/app/scripts/controllers/imin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('foodroulette')
+	.controller('ImInCtrl', ['$scope', '$location','FRUser', 'PATHS'
+		function($scope, $location, FRUser, PATHS) {
+			// Are we logged in? if not we move to login screen
+			FRUser.get().error(function(user) {
+				$location.path(PATHS.LOGIN);
+			});
+		}
+	]);
--- yeoman/app/scripts/services/user.js
+++ yeoman/app/scripts/services/user.js
@@ -1,15 +1,45 @@
 angular.module('foodroulette')
-.factory('FRUser', ['$http', function ($http) {
+.factory('FRUser', ['$http', 'CONFIG' function ($http, CONFIG) {
   var md5,
       me;
 
+  function getMe() {
+    return me || me = $http.get({url: CONFIG.backend + "/user/me"});
+  }
+
+  function setMe(mod_me) {
+    return me = mod_me;
+  }
+
+  function getUser(id) {
+    return $http.get({url: CONFIG.backend + "/user/" + id});
+  }
+
+  function sendToken(token) {
+    return $http.post({
+      url: CONFIG.backend + "/user/register",
+      data: {
+        token: token
+      }
+    });
+  }
+
+  function update() {
+    return $http.post({
+      url: CONFIG.backend + "/user/me",
+      data: me
+    });  
+  }
+
   return {
-    md5: md5,
     // if id is null returns 'me'
-    getUser: function(id) {},
+    get: getMe,
+    set: setMe,
+    getUser: getUser,
     // registers token
-    sendToken: function(token) {},
-    // posts me
-    postUser: function() {}
+    sendToken: sendToken,
+    /* update - updates information contained in the service */ 
+    update: update
   };
+  
 }])
