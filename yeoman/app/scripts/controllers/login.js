1b880479c08d0b90355cf2d34e074ae2187c9756
Jesus F. Trujillo <elyeyus@gmail.com>
2013-11-16 21:52:58 -0800

    fix sigin flow

--- yeoman/app/scripts/controllers/login.js
+++ yeoman/app/scripts/controllers/login.js
@@ -5,7 +5,7 @@ angular.module('foodroulette')
 			if($routeParams.md5) {
 				console.log("md5 received from Yammer: " + $routeParams.md5);
 				localStorageService.add('md5',$routeParams.md5);
-				$location.path(PATHS.IMIN);
+				$location.path(PATHS.INTERESTS);
 			}
 
 		}
